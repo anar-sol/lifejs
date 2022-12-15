e401276f1f7dfefc5a90e4f82b5f52295b0e5aa4
Anar <anar.sol.dev@gmail.com>
2022-12-15 23:08:41 +0100

    first commit

---
+++ tests/life.test.js
@@ -0,0 +1,17 @@
+import { Life } from "../src/life.js";
+
+describe("Life", () => {
+
+    const COLUMNS = 20;
+    const ROWS = 20;
+    let life;
+
+    beforeEach(() => {
+        life = Life.newLife(COLUMNS, ROWS);
+    });
+
+    test("has columns and rows properties", () => {
+        expect(life.columns).toBe(COLUMNS);
+        expect(life.rows).toBe(ROWS);
+    });
+});
