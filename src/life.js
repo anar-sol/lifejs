33a09ec6b9cc2b7ff612037c59d7b0d35dafe7f3
Anar <anar.sol.dev@gmail.com>
2022-12-16 00:12:41 +0100

    implement isCellAlive and setCellState

--- src/life.js
+++ src/life.js
@@ -1,11 +1,16 @@
 class Life {
 
+    static CELL_STATE_ALIVE = true;
+    static CELL_STATE_DEAD = false;
+
     #columns;
     #rows;
+    #grid;
 
     constructor(columns, rows) {
         this.#columns = columns;
         this.#rows = rows;
+        this.#initGrid(columns, rows);
     }
 
     static newLife(columns, rows) {
@@ -18,6 +23,22 @@ class Life {
 
     get rows() {
         return this.#rows;
+    }
+
+    setCellState(col, row, state) {
+        this.#grid[col][row] = state;
+    }
+
+    isCellAlive(col, row) {
+        return this.#grid[col][row];
+    }
+
+    #initGrid(columns, rows) {
+        this.#grid = new Array(columns);
+        for (let col = 0; col < columns; col++) {
+            this.#grid[col] = new Array(rows);
+            this.#grid[col].fill(Life.CELL_STATE_DEAD);
+        }
     }
 }
 
--- tests/life.test.js
+++ tests/life.test.js
@@ -14,4 +14,69 @@ describe("Life", () => {
         expect(life.columns).toBe(COLUMNS);
         expect(life.rows).toBe(ROWS);
     });
+
+    test("isCellAlive (1, 1)", () => {
+        const col = 1;
+        const row = 1;
+
+        expect(life.isCellAlive(col, row)).toBe(false);
+
+        life.setCellState(col, row, Life.CELL_STATE_ALIVE);
+        expect(life.isCellAlive(col, row)).toBe(true);
+
+        life.setCellState(col, row, Life.CELL_STATE_DEAD);
+        expect(life.isCellAlive(col, row)).toBe(false);
+    });
+
+    test("isCellAlive (0, 0)", () => {
+        const col = 0;
+        const row = 0;
+
+        expect(life.isCellAlive(col, row)).toBe(false);
+
+        life.setCellState(col, row, Life.CELL_STATE_ALIVE);
+        expect(life.isCellAlive(col, row)).toBe(true);
+
+        life.setCellState(col, row, Life.CELL_STATE_DEAD);
+        expect(life.isCellAlive(col, row)).toBe(false);
+    });
+
+    test("isCellAlive (columns - 1, 0)", () => {
+        const col = COLUMNS - 1;
+        const row = 0;
+
+        expect(life.isCellAlive(col, row)).toBe(false);
+
+        life.setCellState(col, row, Life.CELL_STATE_ALIVE);
+        expect(life.isCellAlive(col, row)).toBe(true);
+
+        life.setCellState(col, row, Life.CELL_STATE_DEAD);
+        expect(life.isCellAlive(col, row)).toBe(false);
+    });
+
+    test("isCellAlive (0, rows - 1)", () => {
+        const col = 0;
+        const row = ROWS - 1;
+
+        expect(life.isCellAlive(col, row)).toBe(false);
+
+        life.setCellState(col, row, Life.CELL_STATE_ALIVE);
+        expect(life.isCellAlive(col, row)).toBe(true);
+
+        life.setCellState(col, row, Life.CELL_STATE_DEAD);
+        expect(life.isCellAlive(col, row)).toBe(false);
+    });
+
+    test("isCellAlive (columns - 1, rows - 1)", () => {
+        const col = COLUMNS - 1;
+        const row = ROWS - 1;
+
+        expect(life.isCellAlive(col, row)).toBe(false);
+
+        life.setCellState(col, row, Life.CELL_STATE_ALIVE);
+        expect(life.isCellAlive(col, row)).toBe(true);
+
+        life.setCellState(col, row, Life.CELL_STATE_DEAD);
+        expect(life.isCellAlive(col, row)).toBe(false);
+    });
 });
